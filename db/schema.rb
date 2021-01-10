93d06ddd520f51d0ca53858633e4ac9f969c3276
Julien Bouland <bouland.julien@gmail.com>
2021-01-10 16:15:30 +0100

    Artist scaffolded

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_10_145952) do
+ActiveRecord::Schema.define(version: 2021_01_10_151449) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_artists_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -19,4 +27,5 @@ ActiveRecord::Schema.define(version: 2021_01_10_145952) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "artists", "users"
 end
