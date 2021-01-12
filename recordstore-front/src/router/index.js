f4b3e412e40beb4502f5fc07153b322d0e563991
Julien Bouland <bouland.julien@gmail.com>
2021-01-12 12:38:59 +0100

    Router Artists

--- recordstore-front/src/router/index.js
+++ recordstore-front/src/router/index.js
@@ -2,12 +2,18 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import SignIn from '@/components/SignIn.vue'
 import SignUp from '@/components/SignUp.vue'
+import Artists from '@/components/Artists.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
+    {
+      path: '/artist',
+      name: 'Artists',
+      component: Artists
+    },
     {
       path: '/',
       name: 'SignIn',
